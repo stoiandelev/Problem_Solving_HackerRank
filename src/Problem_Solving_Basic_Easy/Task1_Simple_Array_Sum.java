6d3ea5ef232dd4f045151fb76624ce36c37e2016
stoiandelev <stoian.delev@yahoo.com>
2022-12-28 13:38:23 +0200

    Array Sum

--- src/Problem_Solving_Basic_Easy/Task1_Simple_Array_Sum.java
+++ src/Problem_Solving_Basic_Easy/Task1_Simple_Array_Sum.java
@@ -2,30 +2,29 @@ package Problem_Solving_Basic_Easy;
 
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Stream;
-
 
 
 public class Task1_Simple_Array_Sum {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        Scanner scanner = new Scanner(System.in);
 
-        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+        int arrayCount = Integer.parseInt(scanner.next().trim());
 
-        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                .map(Integer::parseInt)
-                .toList();
+        List<Integer> array = new ArrayList<>();
+        for (int i = 0; i < arrayCount; i++) {
+            int number = scanner.nextInt();
+            array.add(number);
+        }
 
-        int result = Result.simpleArraySum(ar);
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
 
-        bufferedReader.close();
-        bufferedWriter.close();
+        int result = Result.simpleArraySum(array);
+        System.out.println(result);
     }
 
 }
